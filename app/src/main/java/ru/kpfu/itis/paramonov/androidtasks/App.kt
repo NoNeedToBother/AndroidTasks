f04af3cb79eef76198f20b190bdb91bd440c7d0b
Paramonov Dmitry <113761691+NoNeedToBother@users.noreply.github.com>
2024-05-27 17:40:57 +0300

    Revert "Hw custom view"

--- app/src/main/java/ru/kpfu/itis/paramonov/androidtasks/App.kt
+++
@@ -1,5 +0,0 @@
-package ru.kpfu.itis.paramonov.androidtasks
-
-import android.app.Application
-
-class App : Application()
---
+++ app/src/main/java/ru/kpfu/itis/paramonov/androidtasks/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.kpfu.itis.paramonov.androidtasks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

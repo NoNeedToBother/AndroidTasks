62d45f4eb16507c242581ba05b34664ea9ee9ed3
Dmitry Paramonov <dmitparam.2017@gmail.com>
2024-05-27 11:36:02 +0300

    graph view

--- app/src/main/java/ru/kpfu/itis/paramonov/androidtasks/ui/fragments/GraphFragment.kt
+++ app/src/main/java/ru/kpfu/itis/paramonov/androidtasks/ui/fragments/GraphFragment.kt
@@ -5,12 +5,20 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import ru.kpfu.itis.paramonov.androidtasks.R
-import ru.kpfu.itis.paramonov.androidtasks.databinding.FragmentMainBinding
+import ru.kpfu.itis.paramonov.androidtasks.databinding.FragmentGraphBinding
 
-class MainFragment : Fragment(R.layout.fragment_main) {
-    private val viewBinding: FragmentMainBinding by viewBinding(FragmentMainBinding::bind)
+class GraphFragment: Fragment(R.layout.fragment_graph) {
+
+    private val binding: FragmentGraphBinding by viewBinding(FragmentGraphBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    companion object {
+        fun newInstance(): GraphFragment = GraphFragment()
+
+        const val GRAPH_FRAGMENT_TAG = "GRAPH_FRAGMENT"
     }
 }
